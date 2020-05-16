d3ecc299ab8bc5b42d3f1a0164ad9a5c3d6c1dec
Shlok Desai <shlokpdesai@gmail.com>
2020-05-16 12:46:23 -0700

    Init

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import jobsStore from "./domain";
+
+  $: filters = [];
+
+  const addFilter = () => {
+    filters = [...filters, Math.random() * 10];
+  };
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {$jobsStore.length}!</h1>
+  <ul>
+    {#each filters as filter}
+      <li>{filter}</li>
+    {/each}
+  </ul>
+  <button on:click={addFilter}>Add Filter</button>
+</main>
---
+++ src/domain.js
@@ -0,0 +1,4 @@
+import { writable } from "svelte/store";
+import jobsData from "./data";
+
+export default writable(jobsData);
