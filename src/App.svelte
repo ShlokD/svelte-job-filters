dea6662d4961f5959fb9a065a463b0789b1435bb
Shlok Desai <shlokpdesai@gmail.com>
2020-05-17 15:39:36 -0700

    fix styling and filtering

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,19 @@
 <script>
-  import jobsStore from "./domain";
+  import { jobsStore } from "./domain";
 
-  $: filters = [];
+  let selectedFilters = [];
 
-  const addFilter = () => {
-    filters = [...filters, Math.random() * 10];
+  function doFilter(filter) {
+    selectedFilters = [...selectedFilters, filter];
+    jobsStore.doFilterBy(selectedFilters);
+  }
+
+  const removeFilter = filter => {
+    const selectedFilterIndex = selectedFilters.indexOf(filter);
+    selectedFilters = selectedFilters
+      .slice(0, selectedFilterIndex)
+      .concat(selectedFilters.slice(selectedFilterIndex + 1));
+    jobsStore.doFilterBy(selectedFilters);
   };
 </script>
 
@@ -12,30 +21,126 @@
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+    margin: 0.6em;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  .listing {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 2em 4em;
+    margin: 2em 0;
+    position: relative;
+
+    min-width: 80%;
+    background-color: #fefefe;
+    box-shadow: 0px 0px 5px 5px var(--light-grayish-cyan-2);
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
+  .listing > img {
+    margin: 0 1em;
+  }
+
+  .details {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .details > p {
+    color: var(--desaturated-dark-cyan);
+    font-weight: 600;
+  }
+
+  h2 {
+    margin: 0;
+  }
+
+  .metadata {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding-left: 0;
+  }
+
+  .metadata > li {
+    padding-right: 2em;
+    list-style: none;
+    color: lightslategray;
+  }
+
+  .job-filters {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 2em;
+  }
+
+  .job-filters button {
+    margin: 1em;
+    background-color: var(--light-grayish-cyan-2);
+    color: var(--desaturated-dark-cyan);
+    border: 0;
+    padding: 1em;
+    cursor: pointer;
+  }
+
+  .selected-filters {
+    display: flex;
+  }
+
+  .selected-filters > button {
+    margin: 1em;
+    background-color: var(--desaturated-dark-cyan);
+    color: var(--light-grayish-cyan);
+    border: 0;
+    padding: 1em;
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 768px) {
+    .listing {
+      flex-direction: column;
+    }
+
+    .details {
+      align-items: center;
     }
   }
 </style>
 
 <main>
-  <h1>Hello {$jobsStore.length}!</h1>
-  <ul>
-    {#each filters as filter}
-      <li>{filter}</li>
+  <div class="selected-filters">
+    {#each selectedFilters as selectedFilter}
+      <button on:click={removeFilter(selectedFilter)}>
+        {selectedFilter} X
+      </button>
     {/each}
-  </ul>
-  <button on:click={addFilter}>Add Filter</button>
+  </div>
+  {#each $jobsStore as job}
+    <div class="listing">
+      <img alt={`${job.company}-logo`} src={job.logo} />
+      <div class="details">
+        <p>{job.company}</p>
+        <h2>{job.position}</h2>
+        <ul class="metadata">
+          <li>{job.postedAt}</li>
+          <li>{job.contract}</li>
+          <li>{job.location}</li>
+        </ul>
+
+      </div>
+
+      <div class="job-filters">
+        {#each job.filters as filter}
+          <button on:click={doFilter(filter)}>{filter}</button>
+        {/each}
+      </div>
+    </div>
+  {/each}
 </main>
--- src/domain.js
+++ src/domain.js
@@ -1,4 +1,53 @@
 import { writable } from "svelte/store";
 import jobsData from "./data";
 
-export default writable(jobsData);
+String.prototype.toTitleCase = function () {
+  if (!this.length) return;
+
+  return this[0].toUpperCase() + this.substring(1, this.length).toLowerCase();
+};
+
+export const transformJobsData = (jobs) => {
+  return jobs.map((job) => {
+    const { languages = [], level, role } = job;
+    return {
+      filters: [...languages, level.toTitleCase(), role.toTitleCase()],
+      ...job,
+    };
+  });
+};
+
+const intersection = (arr1, arr2) => {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  const intersect = [...set1].filter((elem) => set2.has(elem));
+  return [...intersect];
+};
+
+const createJobsStore = () => {
+  const initialJobs = transformJobsData(jobsData);
+  const { subscribe, update } = writable(initialJobs);
+
+  const doFilterBy = (filters) =>
+    update(() => {
+      if (!filters.length) return initialJobs;
+
+      let filteredJobs = initialJobs.slice().filter((job) => {
+        // Since the intersection of a set and a subset is the subset itself,
+        // if a particular job matches all the set filters, then the length
+        // of the intersection of the job filters with the selected filters
+        // will be the filter length itself
+        return intersection(job.filters, filters).length === filters.length;
+      });
+
+      return filteredJobs;
+    });
+
+  return {
+    subscribe,
+    doFilterBy,
+  };
+};
+
+export const jobsStore = createJobsStore();
